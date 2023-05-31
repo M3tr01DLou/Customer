dfb51a9f5c0bef96e4bca69e38a49258c312255f
PANEL\luisjavier.diez <luisjavier.diez@panel.es>
2023-05-31 11:06:51 +0200

    Add ASP Controller

---
+++ Customer.API/Controllers/AspsController.cs
@@ -0,0 +1,100 @@
+﻿using Customer.API.Data;
+using Customer.Common.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace Customer.API.Controllers
+{
+    [ApiController]
+    [Route("/api/asps")]
+    public class AspsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AspsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAllAsps")]
+        public async Task<IActionResult> GetAspsAsync()
+        {
+            return Ok(await _context.Asps.ToListAsync());
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAspAsync(int id)
+        {
+            var asp = await _context.Asps.FindAsync(id);
+            if(asp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(asp);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(Asp asp)
+        {
+            try
+            {
+                _context.Add(asp);
+                await _context.SaveChangesAsync();
+                return Ok(asp);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe una asp con el mismo nombre.");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Asp asp)
+        {
+            try
+            {
+                _context.Update(asp);
+                await _context.SaveChangesAsync();
+                return Ok(asp);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                {
+                    return BadRequest("Ya existe una asp con el mismo nombre.");
+                }
+
+                return BadRequest(dbUpdateException.Message);
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var asp = await _context.Asps.FirstOrDefaultAsync(x => x.Id == id);
+            if (asp == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(asp);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
